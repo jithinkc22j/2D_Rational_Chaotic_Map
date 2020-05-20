799e52ad62e47d247fcf44112b963c640ff8f54e
JITHIN K C <jithinkc22@gmail.com>
2020-05-20 17:26:55 +0530

    Add files via upload

---
+++ rational_2D.m
@@ -0,0 +1,18 @@
+function out=rational_2D(x,y,a,b,siz)
+
+n=siz*siz;
+
+for i=1:n
+
+   % if you need complete code contact me
+   
+   
+end
+
+x1= reshape(x1,[siz,siz]);
+
+y1= reshape(y1,[siz,siz]);
+%plot(x1,y1,'DisplayName','y1');
+out=x1;
+end
+
